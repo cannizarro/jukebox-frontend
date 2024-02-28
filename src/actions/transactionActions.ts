1765426d64dc256892a7011e41a42922ef8d6188
asrarhaque <asrarul97@gmail.com>
2024-02-28 05:37:28 +0530

    transaction page fixed

--- src/actions/transactionActions.ts
+++ src/actions/transactionActions.ts
@@ -1,8 +1,12 @@
 import { invokeRestApi } from "../api/axiosHelper";
 import { TransactionPageRequestType } from "../reducers/transactionReducer";
-import { ActionType, TRANSACTION_ACTION_FAILURE, TRANSACTION_ACTION_SUCCESSFULL } from "./constants/actionTypes";
+import { ActionType, TRANSACTION_ACTION_FAILURE, TRANSACTION_ACTION_SUCCESSFULL, TRANSACTION_LOADING } from "./constants/actionTypes";
 
 export function getTransactions(requestBody: TransactionPageRequestType, dispatch: React.Dispatch<ActionType>){
+
+	dispatch({
+		type: TRANSACTION_LOADING
+	} as ActionType);
 
 	invokeRestApi(
 		"get",
